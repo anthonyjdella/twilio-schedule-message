bc720e3fad58c2b82725f0f82dadd38e69c2fa1f
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2023-03-10 15:33:32 -0800

    refactor(app.py): try/except block and removing code

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 @app.route("/v1/message/schedule", methods=["POST"])
 def send_scheduled_texts():
-    data = request.get_json()
-    to_number = data.get("number")
-    minutes_ahead = data.get("minutes")
-    message = data.get("message")
-
     try:
+        data = request.get_json()
+        to_number = data.get("number")
+        minutes_ahead = data.get("minutes")
+        message = data.get("message")
+
         schedule_message(to_number, minutes_ahead, message)
         return Response('{"status": "Message sent successfully"}', status=201, mimetype='application/json')
     except Exception as e:
@@ -23,5 +23,4 @@ def send_scheduled_texts():
         return Response('{"status": "Message was not sent"}', status=500, mimetype='application/json')
 
 
-if __name__ == "__main__":
-    app.run(host='localhost', port=3000)
+app.run(host='localhost', port=3000)
